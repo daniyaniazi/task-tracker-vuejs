9b9e7c0627c3f25b1cf96fca7fec1859db10e131
Daniya Niazi <farihadania@hotmail.com>
2021-09-15 21:06:18 +0500

    props passing

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  // props: ["title"],
+  props: {
+    // title: String,
+    title: {
+      type: String,
+      default: "Hello Vue",
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
